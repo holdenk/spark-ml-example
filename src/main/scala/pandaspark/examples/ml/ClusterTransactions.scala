01bea8bf39ab5113ae6cbd50f8b592fee38435b0
Holden Karau <holden@pigscanfly.ca>
2013-11-30 01:42:18 -0800

    Just starting the skeleton

---
+++ src/main/scala/pandaspark/examples/ml/ClusterTransactions.scala
@@ -0,0 +1,12 @@
+package pandaspark.examples
+
+import scala.math
+
+import com.snowplowanalytics.maxmind.geoip.IpGeo
+
+object ClusterTransactions {
+  
+  def main(args: Array[String]) {
+
+  }
+}
